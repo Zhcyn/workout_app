261b20ae0230283f1d078409dfa72fa358cf2044
Liel Titelbaum <liel.titelbaum1@gmail.com>
2021-08-12 18:41:26 +0300

    Initial Commit

---
+++ workout_sency/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  workout_sency
+//
+//  Created by Liel Titelbaum on 12/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
